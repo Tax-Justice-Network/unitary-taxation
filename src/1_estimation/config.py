b24c5498007455224824a0ec41a3c8627b802f81
Daniel Coll <daniel.coll.sol@gmail.com>
2023-06-13 10:18:17 +0200

    fix: Set path for METR analysis using tjn_internal package

--- src/1_estimation/config.py
+++ src/1_estimation/config.py
@@ -6,7 +6,7 @@ YEAR_SOTJ = 2023
 
 
 # Input datasets - Notebook 1 - Section 1
-CBCR_FILE = "../../data/raw/estimations/CBCR_TABLEI_17112022170811646.csv"
+CBCR_FILE = "../../data/raw/estimations/CBCR_TABLEI_12062023113505025.csv"
 ORBIS_FILE = "../../data/raw/estimations/orbis.xlsx"
 CORPORATE_TAX_RATE_PATH = "../../data/raw/estimations/221124 Corporate tax rates.csv"
 # Input datasets - Notebook 1 - Section 2
@@ -16,7 +16,7 @@ BILATERAL_CROSS = f"{tjn_internal.paths.final_data}/20210810_bilateral-year-leve
 GRAV_FILE = f"{tjn_internal.paths.source_data}/022 CEPII/other/Gravity_V202102.dta"
 LINK_FILE = f"{tjn_internal.paths.source_data}/016 Papers/garcia-stausholm2020/combined_data_imputed copy.tsv"
 # Path for METR analysis - [I will adapt the structure of my notebook to also include every single input file here based on tjn_internal in the next days]
-path = "C:/Users/AlisonSchultz/Tax Justice Network Ltd/TJN - Shared Documents/Workstreams/Scale of Tax Injustice/METR/2023"
+path = f"{tjn_internal.paths.sharepoint_root}/TJN - Shared Documents/Workstreams/Scale of Tax Injustice/METR/2023"
 # Path for wealth tax estimates
 WEALTH_TAX = f"{tjn_internal.paths.sharepoint_root}/TJN - Shared Documents/Workstreams/Scale of Tax Injustice/Wealth tax/2_data"
 WEALTH_TAX_INPUT = f"{WEALTH_TAX}/1_input"
