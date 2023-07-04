7b6a1c567691429616fdd9d3994e64de262d0b58
Alison Schultz <alisonschultz@posteo.de>
2023-07-04 09:05:17 +0200

    new config file for Mirek

---
+++ src/1_estimation/config_new.py
@@ -0,0 +1,20 @@
+import tjn_tools
+
+# Set most recent cbcr year and its dataset here https://stats.oecd.org/Index.aspx?DataSetCode=CBCR_TABLEI
+YEAR_CBCR = 2018
+YEAR_SOTJ = 2023
+
+
+# Input datasets - Notebook 1 - Section 1
+CBCR_FILE = "../../data/raw/estimations/CBCR_TABLEI_20062023110215400.csv"
+ORBIS_FILE = "../../data/raw/estimations/orbis.xlsx"
+CORPORATE_TAX_RATE_PATH = "../../data/raw/estimations/221124 Corporate tax rates.csv"
+# Input datasets - Notebook 1 - Section 2
+UNILATERAL_CROSS = f"{tjn_tools.paths.final_data}/20230615_unilateral_cross.csv"
+UNILATERAL_PANEL = f"{tjn_tools.paths.final_data}/20230613_unilateral_panel.csv"
+BILATERAL_CROSS = f"{tjn_tools.paths.final_data}/20230613_bilateral_cross.csv"
+BILATERAL_PANEL = f"{tjn_tools.paths.final_data}/20230101_bilateral_panel.csv"
+GRAV_FILE = f"{tjn_tools.paths.source_data}/022 CEPII/other/Gravity_V202102.dta"
+LINK_FILE = f"{tjn_tools.paths.source_data}/016 Papers/garcia-stausholm2020/combined_data_imputed copy.tsv"
+# Path for METR analysis - Notebook 4
+metr_path = f"{tjn_tools.paths.sharepoint_root}/TJN - Shared Documents/Workstreams/Scale of Tax Injustice/METR/2023"
